5de8babd3f0eb45169a8f75031f8d844046f918d
Otto Domination <otto@automationdomination.me>
2017-07-19 21:18:25 -0500

    revd up

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-
+        'Programming Language :: Python :: 3.0',
     ]
 )
